9d2ff6bf2e11ea5ee244d4316680dcf9dbd10604
Ben <109313313+10BenAgain@users.noreply.github.com>
2025-01-02 14:45:56 -0700

    Refactor

--- main.c
+++ main.c
@@ -3,16 +3,21 @@
 
 #include "include/commands.h"
 
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        commandHelp();
-        return EXIT_FAILURE;
-    }
+#define PROG_NAME "bin"
 
-    for (size_t i = 0; i < COMMANDS_COUNT; i++) {
-        if (COMATCH(argv[1], commands[i].command)) {
-            commands[i].handler(argc, argv);
-            return EXIT_SUCCESS;
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Unable to process request. Use '%s help' for a list of commands\n", PROG_NAME);
+        return EXIT_FAILURE;
+    } else if (argc < 3 && COMATCH(argv[1], "help")) {
+        commandHelp();
+        return EXIT_SUCCESS;
+    } else {
+        for (size_t i = 0; i < COMMANDS_COUNT; i++) {
+            if (COMATCH(argv[1], commands[i].command)) {
+                commands[i].handler(argc, argv);
+                return EXIT_SUCCESS;
+            }
         }
     }
 
